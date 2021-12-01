fb771e284b82cedf2949c8d3d291442de19891e0
ihebkhadhri <ihebnassreddinekhadhri@gmail.com>
2021-12-01 17:03:00 +0100

    creation components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+import { PlatComponent } from './plat/plat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantComponent,
+    ReservationComponent,
+    HomeComponent,
+    ContactComponent,
+    MenuComponent,
+    PlatComponent
   ],
   imports: [
     BrowserModule,
