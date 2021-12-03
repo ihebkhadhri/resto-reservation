7d4dc23d306286d54de1756046ee1396c7fe9ba7
hedihorry21 <hedihorry21@gmail.com>
2021-12-03 05:11:19 +0100

    reservation

--- src/app/reservation/reservation/reservation.component.ts
+++ src/app/reservation/reservation/reservation.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+
+
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.css']
 })
 export class ReservationComponent implements OnInit {
+  minDate = new Date();
 
-  constructor() { }
+  loading = true;
 
-  ngOnInit(): void {
+  constructor(
+
+    private route: ActivatedRoute) { 
   }
+  ngOnInit() {
+ 
 
+  }
 }
