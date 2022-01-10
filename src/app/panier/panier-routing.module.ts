ff70676612d7223a6ac4357b08ede3c049d3cb06
ihebkhadhri <ihebnassreddinekhadhri@gmail.com>
2022-01-10 22:50:14 +0100

    commit

--- src/app/panier/panier-routing.module.ts
+++ src/app/panier/panier-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PlatComponent } from './plat.component';
+import { PanierComponent } from './panier.component';
 
-const routes: Routes = [{ path: '', component: PlatComponent }];
+const routes: Routes = [{ path: '', component: PanierComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class PlatRoutingModule { }
+export class PanierRoutingModule { }
--- src/app/plat/plat.component.ts
+++ src/app/plat/plat.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { plats } from "./plats-list";
+import { PanierService } from '../services/panier.service';
+import { PlatService } from '../services/plat.service';
+//import { plats } from "./plats-list";
 @Component({
   selector: 'app-plat',
   templateUrl: './plat.component.html',
   styleUrls: ['./plat.component.css']
 })
 export class PlatComponent implements OnInit {
-  public platsList = plats;
+  public platsList: any = [];
 
-  constructor() { }
+  constructor(
+    private panierService: PanierService,
+    private platService: PlatService
+  ) { }
 
   ngOnInit(): void {
-    console.log(this.platsList);
+    this.platService.all().subscribe(
+      res => this.platsList = res
+    );
+  }
+
+  public addToPanier(id: string) {
+    this.panierService.add(id);
   }
 
 }
